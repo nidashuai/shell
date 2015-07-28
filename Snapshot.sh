3a2cb0d52bfed11ffeba77b6fe66d10c95e8bef9
nidashuai <nds521@126.com>
2015-07-28 15:01:53 +0800

    Update Snapshot.sh

--- Snapshot.sh
+++ Snapshot.sh
@@ -21,11 +21,11 @@ snap_ts=$filepath"tablespace.snap"
 snap_tb=$filepath"table.snap"
 
 # Get a snapshot for database and put in $snaps.
-db2 get snapshot for database on idbyf > $snap_database
-db2 get snapshot for locks on idbyf > $snap_locks
-db2 get snapshot for dynamic sql on idbyf > $snap_dsql
-db2 get snapshot for bufferpools on idbyf > $snap_bp
-db2 get snapshot for tablespaces on idbyf > $snap_ts
-db2 get snapshot for tables on idbyf > $snap_tb
+db2 get snapshot for database on database > $snap_database
+db2 get snapshot for locks on database > $snap_locks
+db2 get snapshot for dynamic sql on database > $snap_dsql
+db2 get snapshot for bufferpools on database > $snap_bp
+db2 get snapshot for tablespaces on database > $snap_ts
+db2 get snapshot for tables on database > $snap_tb
 
 #rm $snaps
