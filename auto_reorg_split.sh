cdb5fecbde45aa8f0a9fc69a20c70f9dca5cae56
nidashuai <nds521@126.com>
2015-07-28 14:59:46 +0800

    Update auto_reorg_split.sh

--- auto_reorg_split.sh
+++ auto_reorg_split.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/sh
 #
 #
+
+#定义变量
+TT=`/usr/bin/date +"%Y%m%d%H%M%S"`; export TT
+. /home/dba/sqllib/db2profile
+cd /home/dba/reorg
+
 #生成reorg.txt
 db2 connect to database  
 db2 reorgchk current statistics on table all > reorg.txt
-db2 terminate
-
-#定义变量
-
 
 #将reorg.txt文件分割成reorg_table.txt和reorg_index.txt
 awk '{
@@ -17,7 +19,7 @@ awk '{
      
 awk '/Index statistics:/,0' reorg.txt > reorg_index.txt
 
-#在reorg_table.txt中查找需要重组的表
+#在reorg_table.txt中查找表需要重组的表
 awk '{
       a[NR]=$0;
       if(/\*/)              
@@ -33,7 +35,7 @@ awk '{
        }
 }' reorg_table.txt >reorg_table_list.txt
 
-#在index_table.txt中查找需要重组的表
+#在index_table.txt中查找索引需要重组的表
 awk '{ if(/\*/) print $0;print NR; }' reorg_index.txt >reorg_index_list.txt
 awk '{
       a[NR]=$0;
@@ -55,6 +57,13 @@ awk '{
 grep 'Table:' reorg_table_list.txt| cut -d : -f 2 |sort |uniq > Table.txt
 grep 'Table:' reorg_index_list.txt| cut -d : -f 2 |sort |uniq > Index.txt
 
-#生成重组脚本
+#生成重组sql脚本
 awk '{printf "reorg indexes all for table %s allow write access;\n",$0}' Index.txt > reorg_index.sql
 awk '{printf "reorg table %s inplace allow write access;\n",$0}' Table.txt > reorg_table.sql
+
+#执行sql脚本
+db2 -vtf reorg_table.sql > auto_reorg_$TT.msg
+db2 -vtf reorg_index.sql > auto_reorg_$TT.msg
+
+#关闭数据库连接
+db2 terminate
