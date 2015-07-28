36c9e994f30b42fe8b197951091ceaf0e501d842
nidashuai <nds521@126.com>
2015-07-28 14:54:21 +0800

    Update auto_reorg_split.sh

--- auto_reorg_split.sh
+++ auto_reorg_split.sh
@@ -5,7 +5,10 @@
 db2 connect to database  
 db2 reorgchk current statistics on table all > reorg.txt
 db2 terminate
+
 #定义变量
+
+
 #将reorg.txt文件分割成reorg_table.txt和reorg_index.txt
 awk '{
        print $0;
@@ -29,6 +32,7 @@ awk '{
         }
        }
 }' reorg_table.txt >reorg_table_list.txt
+
 #在index_table.txt中查找需要重组的表
 awk '{ if(/\*/) print $0;print NR; }' reorg_index.txt >reorg_index_list.txt
 awk '{
@@ -48,8 +52,8 @@ awk '{
 
 
 #去除重复的表名及不需要的字段     
-grep 'Table:' reorg_table_list.txt| grep -v TBDESQL|grep -v TBIB02LOG|grep -v  TBIPTDOK|cut -d : -f 2 |sort |uniq > Table.txt
-grep 'Table:' reorg_index_list.txt| grep -v TBDESQL|grep -v  TBIB02LOG|grep -v  TBIPTDOK| cut -d : -f 2 |sort |uniq > Index.txt
+grep 'Table:' reorg_table_list.txt| cut -d : -f 2 |sort |uniq > Table.txt
+grep 'Table:' reorg_index_list.txt| cut -d : -f 2 |sort |uniq > Index.txt
 
 #生成重组脚本
 awk '{printf "reorg indexes all for table %s allow write access;\n",$0}' Index.txt > reorg_index.sql
